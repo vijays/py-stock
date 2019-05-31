576e84727f441a573b0be54b1f5e159c4afad200
Vijay <vijay@MBA.local>
2019-05-31 21:41:54 +0530

    Define date range, get data, print first 10 records

--- py-stock.py
+++ py-stock.py
@@ -3,3 +3,15 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
+
+style.use('ggplot')
+
+# define date range
+start=dt.datetime(2018,1,1)
+end=dt.datetime(2018,12,31)
+
+# get data for defined date range
+df=web.DataReader('TSLA','yahoo',start,end)
+
+# print first 10 records
+print(df.head(10))
