c9b4220ddaa959e2d8270554de5b4bb9afe5778a
Vijay <vijays@users.noreply.github.com>
2019-06-02 08:01:44 +0530

    Plot moving average defining axis

--- py-stock.py
+++ py-stock.py
@@ -10,7 +10,6 @@ style.use('ggplot')
 start=dt.datetime(2018,1,1)
 end=dt.datetime(2018,12,31)
 
-# df is short for data frame
 # get data for defined date range
 df=web.DataReader('TSLA','yahoo',start,end)
 
@@ -26,5 +25,14 @@ df.dropna(inplace=True)
 print(df.head(10))
 
 # plot graph
-df.plot()
+
+#df.plot()
+
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (0,0), rowspan=1, colspan=1, sharex=ax1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.plot(df.index, df['Volume'])
+
 plt.show
